aed9244a6ed9cfef4371f230305fe97d84800835
Saif Husain <148823252+saifhusain62@users.noreply.github.com>
2025-07-27 22:27:10 +0600

    Union define

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+"use strict";
+let person = {
+    name: "saif",
+    id: 102,
+};
+console.log(person);
+const up = "saif";
+console.log(up);
+let a = [];
+a.push("asif");
+console.log(a);
+//  UNION
+let b = [];
+b.push(12, "taif");
+console.log(b);
+// TYPE DEFINE
+let c;
+c = {
+    name: "tanvir",
+    id: 109,
+    mail: "saimumsaif62@gmail.com"
+};
+console.log(c);
--- script.ts
+++ script.ts
@@ -1,1 +1,45 @@
-console.log("Hello World");
+let person = {
+    name:"saif",
+    id:102,
+}
+
+console.log(person)
+
+const up= "saif"
+
+console.log(up)
+
+let a:string[] = []
+
+a.push("asif")
+
+console.log(a);
+
+
+//  UNION
+
+let b:(number | string)[] = []
+
+b.push(12,"taif")
+
+console.log(b)
+
+
+// TYPE DEFINE
+
+ let c:{
+
+    name: string,
+    id: number,
+    mail:string,
+
+}
+
+c = {
+    name:"tanvir",
+    id:109,
+    mail:"saimumsaif62@gmail.com"
+
+}
+
+console.log(c)
